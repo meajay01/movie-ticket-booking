e8a1ad04a050dd52c34d9cbc067b938874be6ddf
Ajay Rajbhar <ajayrajbhar880809@gmail.com>
2026-09-05 18:11:27 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,8 +12,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+      const response = await axios.post("https://movie-ticket-booking-4469.onrender.com/api/auth/login",
         {
           email,
           password,
